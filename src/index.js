8f838885d2d2dbad7695f4e7d4f4d0504dd9abcc
AnsonAMS <anson_tohata@hotmail.com>
2020-10-29 18:26:58 +0100

    Adding Hello.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 // import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'))
+ReactDOM.render(<Hello />, document.getElementById('root'))
 
 // registerServiceWorker();
 
