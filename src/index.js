0caeb6d381e35e5e388963965c13e822c02e5751
AnsonAMS <anson_tohata@hotmail.com>
2020-10-31 16:30:35 +0100

    Adding App component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList.js';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots.js'
 // import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render( 
-            <CardList robots={robots}/>
-  , document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // registerServiceWorker();
 
