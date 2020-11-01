73b0b78d55d3b45572ab0aa1f578d70202a56e61
AnsonAMS <anson_tohata@hotmail.com>
2020-11-01 21:06:11 +0100

    Updating Searchbox and App files

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots.js';
 
-
-
 class App extends Component {
     constructor() {
+        super()
         this.state = {
             robots: robots,
             searchfield: ''
-            
         }
     }
-    render () {
-        return (
-            <div className='tc'>
-                <h1>Robofriends</h1>
-                <SearchBox />
-                <CardList robots={robots}/>
-            </div>
+
+onSearchChange = (event) => {
+    this.setState( {searchfield: event.target.value })
+    const filteredRobots = this.state.robots.filter(robots =>{
+        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase)
+    })
+    console.log(filteredRobots);
+}
+
+render () {
+    return (
+    <div className='tc'>
+        <h1>Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={this.state.robots} />        
+        </div>
 
         );
     }
