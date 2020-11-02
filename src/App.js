75b99038cca49ef4bd6b7519ed1b4b3dce37aca1
AnsonAMS <anson_tohata@hotmail.com>
2020-11-02 17:08:04 +0100

    Updating files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots.js';
+import { robots } from './robots';
 
 class App extends Component {
     constructor() {
@@ -13,19 +13,27 @@ class App extends Component {
     }
 
 onSearchChange = (event) => {
-    this.setState( {searchfield: event.target.value })
-    const filteredRobots = this.state.robots.filter(robots =>{
-        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase)
-    })
-    console.log(filteredRobots);
+    this.setState({ searchfield: event.target.value })
 }
 
-render () {
+render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase)
+
+    // const { robots, searchfield } = this.state;
+    // const filteredRobots = robots.filter(robot =>{
+    //     return robot.name.toLowerCase().includes(searchfield.toLowerCase)
+
+    // const { robots, searchfield } = this.state;
+    // const filteredRobots = this.state.robots.filter(robots =>{
+    //     return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase)
+    })
     return (
     <div className='tc'>
         <h1>Robofriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={this.state.robots} />        
+        <CardList robots={filteredRobots} />        
         </div>
 
         );
--- src/index.js
+++ src/index.js
@@ -17,3 +17,13 @@ ReactDOM.render(<App />, document.getElementById('root'))
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './containers/App';
+// import registerServiceWorker from './registerServiceWorker';
+// import 'tachyons'; 
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
